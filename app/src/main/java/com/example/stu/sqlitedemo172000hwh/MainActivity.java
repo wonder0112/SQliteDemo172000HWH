85fcf3de825fea8458db44de757e7eb8099bfd9c
wonder0112 <hwhgdcn@gmail.com>
2019-01-07 09:11:51 +0800

    添加listview控件

--- app/src/main/java/com/example/stu/sqlitedemo172000hwh/MainActivity.java
+++ app/src/main/java/com/example/stu/sqlitedemo172000hwh/MainActivity.java
@@ -4,7 +4,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +18,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     EditText edtTxtName,edtTxtPhone;
     TextView tvDisplay;
+    ListView lvDisplay;
+    List<Infor> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edtTxtName=findViewById(R.id.edtTxt_main_name);
         edtTxtPhone=findViewById(R.id.edtTxt_main_phone);
-        tvDisplay=findViewById(R.id.tv_main_display);
+        //tvDisplay=findViewById(R.id.tv_main_display);
+        lvDisplay=findViewById(R.id.lv_main_display);
         findViewById(R.id.btn_main_insert).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,22 +42,24 @@ public class MainActivity extends AppCompatActivity {
                 adapter.insert(infor);
             }
         });
+        //点击查询
         findViewById(R.id.btn_main_query).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MySQLiteAdapter adapter=new MySQLiteAdapter(getApplicationContext());
-                List<Infor> list=adapter.queryAll();
-                //list
-                Iterator<Infor> iterator=list.iterator();
-                tvDisplay.setText(null);
-                while (iterator.hasNext()){
-                    Infor infor=iterator.next();
-                    tvDisplay.append("ID:"+String.valueOf(infor.get_id()));
-                    tvDisplay.append("name:"+infor.getName());
-                    tvDisplay.append("phone:"+String.valueOf(infor.getPhone())+"\n");
-                }
+                MySQLiteAdapter adapter=new MySQLiteAdapter(getApplicationContext());//定义SQLite适配器对象
+                list=adapter.queryAll();//查询数据，返回值保持到list集合 类对象中
+                lvDisplay.setAdapter(new MyAdapter());//创建一个匿名listview的适配器，并通过setAdapter设置listview的适配器
             }
         });
+        lvDisplay.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+
+
         findViewById(R.id.btn_main_delete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                // Toast.makeText(getApplicationContext(),"删除记录条数："+String.valueOf(num),Toast.LENGTH_SHORT).show();
             }
         });
+
         findViewById(R.id.btn_main_update).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,4 +88,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+//定义用于将集合类对象list的数据赋值到listveiw的适配器
+    class MyAdapter extends BaseAdapter{//BaseAdapter抽象，实现4个方法
+        //实现4个方法，前面3个是处理数据，最后一个是处理视图
+        @Override
+        public int getCount() {
+            return list.size();//记录数量
+        }
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);//数据对象个体
+        }
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view=View.inflate(getApplicationContext(),R.layout.list_item,null);//每行的格式布局文件
+            //获取控件
+            TextView tvID=view.findViewById(R.id.tv_listitem_id);
+            TextView tvName=view.findViewById(R.id.tv_listitem_name);
+            TextView tvPhone=view.findViewById(R.id.tv_listitem_phone);
+            //获取对象
+            Infor infor=(Infor)getItem(position);
+            //给控件赋值
+            tvID.setText(String.valueOf(infor.get_id()));
+            tvName.setText(infor.getName());
+            tvPhone.setText(String.valueOf(infor.getPhone()));
+            return view;
+        }
+    }
+
+
 }
