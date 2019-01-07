58f1a1265f2d0dda53be4fb17549d9c613b2eb02
wonder0112 <hwhgdcn@gmail.com>
2019-01-07 09:58:13 +0800

    添加查询方法

--- app/src/main/java/com/example/stu/sqlitedemo172000hwh/StuSQLiteAdapter.java
+++ app/src/main/java/com/example/stu/sqlitedemo172000hwh/StuSQLiteAdapter.java
@@ -6,6 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StuSQLiteAdapter {
     SQLiteDatabase db;
     Context context;
@@ -45,9 +48,41 @@ public class StuSQLiteAdapter {
     public void update(){
 
     }
-    public void query(){
+    //查询功能，，输出结果：？List<Student>输入参数：？？,无参数：全部记录，仅有姓名：该姓名下的记录，既有姓名也有科目：单条记录
+    public List<Student> queryAll(){
+        List<Student> list=new ArrayList<>();
+        opendDB();
+        //查询所有数据
+        Cursor cursor=db.query(TABLENAME,null,null,null,null,null,null,null);
+        //游标Cursor-->List
+        if(cursor.getCount()>0){
+            cursor.moveToFirst();//把指针指向第一条记录
+            do{
+                int id=cursor.getInt(0);
 
+            }while (cursor.moveToNext());
+        }
+
+
+        closeDB();
+        return  list;
     }
+    public List<Student> querybyName(String name){
+        List<Student> list=new ArrayList<>();
+
+
+        return  list;
+    }
+
+    public List<Student> querybyNameAndCourse(String name,String course){
+        List<Student> list=new ArrayList<>();
+
+
+        return  list;
+    }
+
+
+
     public void delete(){
 
     }
